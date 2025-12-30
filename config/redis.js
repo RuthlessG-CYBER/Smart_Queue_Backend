c67c3e14f1ed88cdfd9fb35d33d1fa3c7ef7cc0f
RuthlessG-CYBER <pandasoumya604@gmail.com>
2025-12-30 21:36:31 +0530

    Smart_Queue_Backend

--- config/redis.js
+++ config/redis.js
@@ -2,17 +2,21 @@ import { createClient } from "redis"
 import "dotenv/config"
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
-})
+  url: process.env.REDIS_URL,
+  socket: {
+    reconnectStrategy: retries => Math.min(retries * 100, 3000),
+  },
+});
 
 redisClient.on("connect", () => {
-  console.log("Redis connected!")
-})
+  console.log("Redis connected!");
+});
 
 redisClient.on("error", (err) => {
-  console.error("Redis error:", err)
-})
+  console.error("Redis error:", err.message);
+});
 
-await redisClient.connect()
+await redisClient.connect();
 
-export default redisClient
+export default redisClient;
+
